c9bb6dd12cb8c4273062efe826a781d6cc8dba6a
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2014-12-20 23:06:57 -0800

    Initial Commit

---
+++ rpg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rpg
+//
+//  Created by Robert Wilkinson on 12/20/14.
+//  Copyright (c) 2014 Robert WIlkinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
